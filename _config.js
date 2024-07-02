eadc62759600d35e0627a1e76b5225bc72a7c110
Jose Muhlanga <muhlangakupeka@outlook.com>
2024-07-02 16:18:15 +0300

    Config file update

--- _config.js
+++ _config.js
@@ -1,9 +1,10 @@
 var config = {}
 
 // Update to have your correct username and password
+
 config.mongoURI = {
-    production: 'mongodb+srv://<muhlangakupeka>:<@Wr8xsd6k>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<muhlangakupeka>:<@Wr8xsd6k>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<muhlangakupeka>:<@Wr8xsd6k>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://<muhlangakupeka>:<testPass1>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://<muhlangakupeka>:<testPass1>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://<muhlangakupeka>:<testPass1>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
