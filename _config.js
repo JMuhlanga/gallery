c4686d989abe84599eae7ef0b938bc5c8deb8eef
Jose Muhlanga <muhlangakupeka@outlook.com>
2024-07-03 22:09:30 +0300

    Some Additions to JenkinFile

--- _config.js
+++ _config.js
@@ -1,6 +1,5 @@
 var config = {}
 
-// Update to have your correct username and password
 config.mongoURI = {
     production: 'mongodb+srv://muhlangakupeka:testPass1@jmcluster.zw7dylm.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://muhlangakupeka:testPass1@jmcluster.zw7dylm.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
